16a7e2d853e4bfe3ccefc60b6438589a1d63f8fc
Denis Defreyne <denis.defreyne@stoneship.org>
2013-09-20 15:53:18 +0200

    Fix test

--- test/filters/test_handlebars.rb
+++ test/filters/test_handlebars.rb
@@ -23,14 +23,14 @@ class Nanoc::Handlebars::FilterTest < Minitest::Test
       :config  => config,
       :content => 'No Payne No Gayne'
     }
-    filter = ::Nanoc::Filters::Handlebars.new(assigns)
+    filter = ::Nanoc::Handlebars::Filter.new(assigns)
 
     # Run filter
-    result = filter.setup_and_run('{{protagonist}} says: {{yield}}.')
+    result = filter.run('{{protagonist}} says: {{yield}}.')
     assert_equal('Max Payne says: No Payne No Gayne.', result)
-    result = filter.setup_and_run('We can’t stop {{item.location}}! This is the {{layout.name}} layout!')
+    result = filter.run('We can’t stop {{item.location}}! This is the {{layout.name}} layout!')
     assert_equal('We can’t stop here! This is the Max Payne layout!', result)
-    result = filter.setup_and_run('It’s raining {{config.animals}} here!')
+    result = filter.run('It’s raining {{config.animals}} here!')
     assert_equal('It’s raining cats and dogs here!', result)
   end
 
